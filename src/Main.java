bac4360e8947d426262da6b6a3e7a545eb36f992
CodeClod <mika9103@stud.kea.dk>
2022-03-02 19:55:31 +0100

    - Encryption properly works now

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+    Scanner in = new Scanner(System.in);
 
     void askForText(){
         System.out.println("Please enter text for encryption: ");
     }
-
-    void askForInt(){
+    void askForShift(){
         System.out.println("Please enter shifting number for encryption: ");
     }
 
@@ -17,33 +17,41 @@ public class Main {
         int[] convertedString = new int[text.length()];
         for (int i = 0; i < text.length(); i++) {
 
-            convertedString[i] = alphabet.indexOf(text.charAt(i));
+            convertedString[i] = alphabet.indexOf(uppercasedString.charAt(i));
         }
         return convertedString;
     }
 
     String intToEncryption(int shift, int[] convertedString) {
-        String alphabet = " ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
-
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+        String encryptedMessage = "";
         for (int i = 0; i < convertedString.length; i++) {
-            if (convertedString[i] == 0) {
-
+            if (convertedString[i] == 0)  {
+                encryptedMessage += " ";
             }
-            convertedString[i] = alphabet.indexOf(text.charAt(i));
+            else {
+                encryptedMessage += (alphabet.charAt(((convertedString[i] + shift ) - 1) % 29));
+            }
         }
+        return encryptedMessage;
     }
 
 
     public static void main(String[] args) {
         Main method = new Main();
-
-        method.askForText();
-
-
-        method.askForInt();
-
-
-
         Scanner in = new Scanner(System.in);
+
+        // Ask user for text and takes input
+        method.askForText();
+        String textToBeEncrypted = in.nextLine();
+
+        // Ask user for shift and take (int) input
+        method.askForShift();
+        int shift = in.nextInt();
+
+
+        String postEncryptionMessage = method.intToEncryption(shift, method.stringToIntConverter(textToBeEncrypted));
+        System.out.println("Your encrypted message is: \n" + postEncryptionMessage);
+
     }
 }
